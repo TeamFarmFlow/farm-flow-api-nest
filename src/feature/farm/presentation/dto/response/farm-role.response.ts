4ae4b1843dbcf5f8653c8625cfea46e891705a91
최원영 <choewy32@gmail.com>
2026-03-07 22:28:26 +0900

    chore: farm list role

--- src/feature/farm/presentation/dto/response/farm-role.response.ts
+++ src/feature/farm/presentation/dto/response/farm-role.response.ts
@@ -5,5 +5,17 @@ import { Expose } from 'class-transformer';
 export class FarmRoleResponse {
   @ApiProperty({ type: String })
   @Expose()
+  id: string;
+
+  @ApiProperty({ type: String })
+  @Expose()
   name: string;
+
+  @ApiProperty({ type: Boolean })
+  @Expose()
+  required: boolean;
+
+  @ApiProperty({ type: Boolean })
+  @Expose()
+  super: boolean;
 }
