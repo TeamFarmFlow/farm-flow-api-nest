d677678a2502c83075230ffd6a73e4efa22ac6ac
최원영 <choewy32@gmail.com>
2026-03-22 16:45:57 +0900

    refactor: infra

--- src/infra/persistence/typeorm/repositories/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './user.repository';
-export * from './user-usage.repository';
-export * from './farm.repository';
-export * from './farm-user.repository';
-export * from './role.repository';
-export * from './role-permission.repository';
-export * from './attendance.repository';
--- src/module/me/me.module.ts
+++ src/module/me/me.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 
-import { TypeOrmExModule, UserEntity, UserRepository } from '@app/infra/persistence/typeorm';
-
 import { ME_USER_REPOSITORY, UpdateMyProfileCommandHandler } from './application';
 import { TypeOrmMeUserRepository } from './infra';
 import { MeController } from './presentation';
 
 @Module({
-  imports: [TypeOrmExModule.forFeature([UserEntity], [UserRepository])],
   controllers: [MeController],
   providers: [
     {
